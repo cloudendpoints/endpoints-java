7ddc3d2053086efccf13c45c36f70f1d19b27455
Clément Denis <clement@altirnao.com>
2016-10-12 17:17:04 +0200

    Use both accounts.google.com and https://accounts.google.com as default issuers for JWT tokens

--- endpoints-framework/src/main/java/com/google/api/server/spi/config/annotationreader/IssuerUtil.java
+++ endpoints-framework/src/main/java/com/google/api/server/spi/config/annotationreader/IssuerUtil.java
@@ -57,6 +57,7 @@ public class IssuerUtil {
     }
     if (!googleAudiencesUnspecified) {
       builder.addIssuerAudiences(Constant.GOOGLE_ID_TOKEN_NAME, googleAudiences);
+      builder.addIssuerAudiences(Constant.GOOGLE_ID_TOKEN_NAME + "_alt", googleAudiences);
     }
     return builder.build();
   }
